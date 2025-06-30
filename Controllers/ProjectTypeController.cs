6fbba87114011ca5502f670e16e2fd70144f181d
suhlaing5 <suhlaingktl5@gmail.com>
2025-06-30 10:47:03 +0630

    combiningProject

---
+++ Controllers/ProjectTypeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Data;
+using ProjectManagementSystem.Models;
+
+namespace ProjectManagementSystem.Controllers
+{
+    public class ProjectTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var projectType = _context.ProjectTypes.ToList();
+            return View(projectType);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var projectType = await _context.ProjectTypes
+                .FirstOrDefaultAsync(m => m.ProjectType_pkId == id);
+            if (projectType == null) return NotFound();
+
+            return View(projectType);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TypeName")] ProjectType projectType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(projectType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(projectType);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var projectType = _context.ProjectTypes.Find(id);
+            if (projectType == null)
+            {
+                return RedirectToAction("Index", "ProjectType");
+            }
+
+            return View(projectType);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ProjectType projectType)
+        {
+            var ptype = _context.ProjectTypes.Find(id);
+
+            if (ptype == null)
+            {
+                return NotFound();
+            }
+
+            ptype.TypeName = projectType.TypeName;
+            _context.SaveChanges();
+            return RedirectToAction("Index", "ProjectType");
+        }
+    }
+
+}
+
--- Models/Project.cs
+++ Models/Project.cs
@@ -12,15 +12,15 @@ namespace ProjectManagementSystem.Models
 
         [Required(ErrorMessage = "Project name is required")]
         [StringLength(200, ErrorMessage = "Project name cannot exceed 200 characters")]
-        public string ProjectName { get; set; }
+        public string ProjectName { get; set; } = "";
 
         [Required(ErrorMessage = "SupervisorName is required")]
         [StringLength(1000, ErrorMessage = "SupervisorName cannot exceed 1000 characters")]
-        public string SupervisorName { get; set; }
+        public string SupervisorName { get; set; } = "";
 
         [Required(ErrorMessage = "Description is required")]
         [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
-        public string Description { get; set; }
+        public string Description { get; set; } = "";
 
         [ForeignKey(nameof(ProjectType))]
         public int ProjectType_pkId { get; set; }
@@ -31,25 +31,25 @@ namespace ProjectManagementSystem.Models
         [ForeignKey(nameof(Framework))]
         public int Framework_pkId { get; set; }
 
-        [ForeignKey(nameof(Company))]
-        public int Company_pkId { get; set; }
+
+        public string CompanyName { get; set; } = "";
 
         public DateTime ProjectSubmittedDate { get; set; } = DateTime.Now;
 
         [StringLength(500, ErrorMessage = "Remark cannot exceed 500 characters")]
-        public string Remark { get; set; }
+        public string Remark { get; set; } = "";
 
         public bool IsDeleted { get; set; } = false;
         public DateTime CreatedDate { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "Creator name is required")]
         [StringLength(100, ErrorMessage = "Creator name cannot exceed 100 characters")]
-        public string CreatedBy { get; set; }
+        public string CreatedBy { get; set; } = "";
 
         public virtual ProjectType ProjectType { get; set; }
         public virtual Language Language { get; set; }
         public virtual Framework Framework { get; set; }
-        public virtual Company Company { get; set; }
+       
         public virtual ICollection<ProjectFile> Files { get; set; }
         public virtual ICollection<ProjectMember> Members { get; set; }
         
